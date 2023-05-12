e35c3a195c3b8dcc0149980e66c699f52689b654
Omar Patel <omarpatel123@yahoo.com>
2023-05-11 17:53:04 -0700

    graphQL added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 
+const { typeDefs, resolvers } = require('./schemas');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 // Body Parsing stores any information coming in into the req.body
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-    console.log(`Database running`);
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
+const startApolloServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        console.log(`Database running`);
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
     })
-})
+};
+
+// Call the async function to start the server
+startApolloServer();
