14688a648761070c0085945181c82233b09042b2
Omar Patel <omarpatel123@yahoo.com>
2023-05-11 17:12:40 -0700

    add model

---
+++ server/models/Profile.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const profileSchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    skills: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+  });
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+db.once('open', () => {
+    console.log(`Database running`);
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    })
 })
